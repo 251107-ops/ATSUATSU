0186265e13beaeb580592216c946418d0a238f1e
Nahuel Grima <251103@st.yoshida-g.ac.jp>
2026-07-09 15:34:31 +0900

    Blueprint追加

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+# routes フォルダの auth.py から auth（Blueprint）をインポート
+from routes.auth import auth
+
+app = Flask(__name__)
+
+# 🔑 セッション（cookie）の暗号化に必要な鍵はここで設定します
+app.secret_key = os.urandom(24)
+
+# 🔌 ここで Blueprint をアプリケーション本体に登録（合体！）
+app.register_blueprint(auth)
+
+if __name__ == "__main__":
+    # 大元からアプリを起動
+    app.run(debug=True)
