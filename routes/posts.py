9ee8b6f3b4b24805e2aacc478fa4c9082eeeea58
Nahuel Grima <251103@st.yoshida-g.ac.jp>
2026-07-13 15:07:38 +0900

    profile遷移２

--- routes/posts.py
+++ routes/posts.py
@@ -13,3 +13,15 @@ def top():
     # result = [dict(row) for row in result]
 
     return render_template('top.html', posts=result)
+
+
+@posts.route("/profile")
+def profile():
+    if 'user_email' not in session:
+        return redirect('/login')
+
+    user_email = session['user_email']
+    db = get_db()
+    user = db.execute("SELECT name, email, department, grade, introduction, icon_path FROM users WHERE email = ?", (user_email,)).fetchone()
+
+    return render_template('profile.html', user=user)
