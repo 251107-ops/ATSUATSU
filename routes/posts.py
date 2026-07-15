94fcc4c30d17fadcb4acda86bb8befe273108687
Nahuel Grima <251103@st.yoshida-g.ac.jp>
2026-07-15 14:31:18 +0900

    新着順機能ちうか

--- routes/posts.py
+++ routes/posts.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect, session, request
 from routes.auth import get_db
+from datetime import datetime
 
 posts = Blueprint('posts', __name__)
 
@@ -17,6 +18,7 @@ def top():
         FROM posts
         JOIN users ON posts.user_id = users.user_id
         JOIN skills ON posts.skill_id = skills.skill_id
+        ORDER BY posts.post_date DESC
     """).fetchall()
 
     posts_list = []
@@ -126,9 +128,10 @@ def create_post():
 
 
         if user_id and skill_id:
+            post_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             db.execute(
-                "INSERT INTO posts (user_id, skill_id, post_type, post_text) VALUES (?, ?, ?, ?)",
-                (user_id, skill_id, post_type, post_text)
+                "INSERT INTO posts (user_id, skill_id, post_type, post_text, post_date) VALUES (?, ?, ?, ?, ?)",
+                (user_id, skill_id, post_type, post_text, post_date)
             )
             db.commit()
             return redirect('/')
